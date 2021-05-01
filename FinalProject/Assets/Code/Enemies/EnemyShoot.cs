3acff5bf6c8786299ffc3ddb5f0775cd2d400b7f
Jordan Foster <jfhawk414@gmail.com>
2021-05-01 16:45:12 -0700

    ranged can shoot and better movement

---
+++ FinalProject/Assets/Code/Enemies/EnemyShoot.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    float shootTime = 7f;
+    float shootDist = 20;
+    private LineRenderer line;
+    bool isShooting = false;
+    Enemy e;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        e = GetComponent<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isShooting)
+            StartCoroutine(shoot());
+    }
+
+    public float getRange()
+    {
+        return shootDist;
+    }
+
+
+    // method to periodically shoot every shootTime seconds
+    // first a line is rendered to act as a targeting system,
+    // then the line changes color to signify it shooting.
+    public IEnumerator shoot()
+    {
+        line.enabled = true;
+        if (isShooting)
+            yield break;
+
+        Debug.Log("SHOT");
+        isShooting = true;
+        
+
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+
+        line.SetPosition(0, transform.position);
+
+
+        if (Physics.Raycast(transform.position, fwd, out hit, shootDist))
+        {
+            Player player = hit.collider.GetComponent<Player>();
+            line.SetPosition(1, hit.point);
+            if (player != null)
+            {
+                player.damage(1);
+            }
+        }
+        else
+        {
+            line.SetPosition(1, transform.position + (fwd * shootDist));
+        }
+
+        yield return new WaitForSeconds(0.5f);
+        line.enabled = false;
+
+        yield return new WaitForSeconds(shootTime);
+        isShooting = false;
+    }
+}
--- FinalProject/Assets/Code/Enemies/MoveRanged.cs
+++ FinalProject/Assets/Code/Enemies/MoveRanged.cs
@@ -11,7 +11,7 @@ public class MoveRanged : MonoBehaviour
     Enemy e;
     float closeDist = 10f;
     float startHeight;
-
+    float weaponRange;
 
     // Start is called before the first frame update
     void Start()
@@ -20,22 +20,20 @@ public class MoveRanged : MonoBehaviour
         e = this.GetComponent<Enemy>();
         dist = transform.position - e.player.transform.position;
         startHeight = transform.position.y;
+        EnemyShoot es = GetComponent<EnemyShoot>();
+        if (es != null)
+            weaponRange = es.getRange();
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = Vector3.zero;
-        if (Vector3.Distance(e.player.transform.position, transform.position) < closeDist)
-        {
-            maintainDist();
-        }
+        //if (Vector3.Distance(e.player.transform.position, transform.position) < closeDist)
+        //{
+        maintainDist();
+        //}
         pointAtPlayer();
-        Debug.Log("TR: " + transform.transform.rotation.eulerAngles.x + ", " + transform.transform.rotation.eulerAngles.z);
-        //if (transform.localEulerAngles.x > 1 || transform.localEulerAngles.z != 0)
-        //    maintainRotation();
-        //else
-        //    pointAtPlayer();
 
         if (transform.position.y != startHeight)
             maintainHeight();
@@ -43,9 +41,6 @@ public class MoveRanged : MonoBehaviour
 
     private void pointAtPlayer()
     {
-        //rb.MovePosition(e.player.transform.position + dist);
-        //transform.position = Vector3.Lerp(transform.position, e.player.transform.position + dist, speed * Time.deltaTime);
-        //transform.LookAt(e.player.transform);
 
         
         Vector3 direction = e.player.transform.position - transform.position;
@@ -60,11 +55,6 @@ public class MoveRanged : MonoBehaviour
 
     private void maintainHeight()
     {
-        RaycastHit hit;
-
-        //if (Physics.Raycast(transform.position, Vector3.down, out hit)) {
-        //    1 + 1;
-        //}
         Vector3 tPos = transform.position;
         Vector3 pos = new Vector3(tPos.x, startHeight, tPos.z);
         transform.position = Vector3.Lerp(tPos, pos, moveSpeed * Time.deltaTime);
@@ -72,19 +62,18 @@ public class MoveRanged : MonoBehaviour
 
     private void maintainDist()
     {
-        Vector3 direction = -1 * transform.forward;
+        Vector3 direction = Vector3.zero;
+        if (Vector3.Distance(e.player.transform.position, transform.position) < closeDist)
+            direction = -1 * transform.forward;
+
+
+
+
+        else if (Vector3.Distance(e.player.transform.position, transform.position) > weaponRange)
+            direction = transform.forward;
+
+
         Vector3 realDirection = new Vector3(direction.x, transform.position.y, direction.z);
-
         rb.AddForce(direction * moveSpeed);
-
-        //transform.position = Vector3.Lerp(transform.position, realDirection, moveSpeed * Time.deltaTime);
-    }
-
-    private void maintainRotation()
-    {
-        //Vector3 direction = e.player.transform.position - transform.position;
-        //transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, lookSpeed * Time.deltaTime);
-        ////Vector3 direction2 = new Vector3(0, direction.transform.rotation.y, 0);
-        //transform.rotation = Quaternion.Euler(0, transform.rotation.y, 0);
     }
 }
