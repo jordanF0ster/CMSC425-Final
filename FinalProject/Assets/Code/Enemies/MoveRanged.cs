f23f82879f14fd497e812659f9000712a27512bc
Jordan Foster <jfhawk414@gmail.com>
2021-05-01 17:43:17 -0700

    added ranged enemies and improved movement

--- FinalProject/Assets/Code/Enemies/MoveRanged.cs
+++ FinalProject/Assets/Code/Enemies/MoveRanged.cs
@@ -4,30 +4,76 @@ using UnityEngine;
 
 public class MoveRanged : MonoBehaviour
 {
-    public Player player;
     Vector3 dist;
-    float speed = 10;
+    float lookSpeed = 2f;
+    float moveSpeed = 50f;
     Rigidbody rb;
-
+    Enemy e;
+    float closeDist = 10f;
+    float startHeight;
+    float weaponRange;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        dist = transform.position - player.transform.position;
+        e = this.GetComponent<Enemy>();
+        dist = transform.position - e.player.transform.position;
+        startHeight = transform.position.y;
+        EnemyShoot es = GetComponent<EnemyShoot>();
+        if (es != null)
+            weaponRange = es.getRange();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = Vector3.zero;
+        //if (Vector3.Distance(e.player.transform.position, transform.position) < closeDist)
+        //{
+        maintainDist();
+        //}
+        pointAtPlayer();
+
+        if (transform.position.y != startHeight)
+            maintainHeight();
+    }
+
+    private void pointAtPlayer()
+    {
+
+
+        Vector3 direction = e.player.transform.position - transform.position;
+        Quaternion toRotation = Quaternion.LookRotation(direction);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, lookSpeed * Time.deltaTime);
+        Quaternion quat = transform.rotation;
+        quat.eulerAngles = new Vector3(0, quat.eulerAngles.y, 0);
+        transform.rotation = quat;
+    }
+
+
+    private void maintainHeight()
+    {
+        Vector3 tPos = transform.position;
+        Vector3 pos = new Vector3(tPos.x, startHeight, tPos.z);
+        transform.position = Vector3.Lerp(tPos, pos, moveSpeed * Time.deltaTime);
     }
 
     private void maintainDist()
     {
-        rb.MovePosition(player.transform.position + (dist * speed * Time.deltaTime));
-        //transform.position = Vector3.Lerp(transform.position, player.transform.position + dist, speed * Time.deltaTime);
-        transform.LookAt(player.transform);
-    }
+        Vector3 direction = Vector3.zero;
+        if (Vector3.Distance(e.player.transform.position, transform.position) < closeDist)
+            direction = -1 * transform.forward;
 
+
+
+
+        else if (Vector3.Distance(e.player.transform.position, transform.position) > weaponRange)
+            direction = transform.forward;
+
+
+        Vector3 realDirection = new Vector3(direction.x, transform.position.y, direction.z);
+        rb.AddForce(direction * moveSpeed);
+    }
 }
