d624804b6e2f67569d3e982147b735f3ce690494
Jordan Foster <jfhawk414@gmail.com>
2021-05-06 10:12:37 -0700

    Fixed issue where start menu not showing

--- FinalProject/Assets/Code/UI/StartMenu.cs
+++ FinalProject/Assets/Code/UI/StartMenu.cs
@@ -13,13 +13,13 @@ public class StartMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void setStartManager(StartManager manager)
